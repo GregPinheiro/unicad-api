025b99910c3b7b1e8e9735e182604b23402e84c4
GregPinheiro <gpinheirojunior@gmail.com>
2021-09-16 18:19:01 -0300

    Add cors lib

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const { json } = require('express');
 const express = require('express');
 const app = express();
 const routers = require('./routers/entregas.routers');
+const cors = require('cors');
 require('dotenv').config();
 
 app.use(express.urlencoded( { extended: false } ));
 app.use(express.json());
+app.use(cors());
 
 app.use('/api/v1', routers);
 
