73f85d7b232157291c095576ca919428c0be65ef
GregPinheiro <gpinheirojunior@gmail.com>
2021-09-17 19:22:50 -0300

    middleware validation

--- middlewares/validation_schema.js
+++ middlewares/validation_schema.js
@@ -1,6 +1,6 @@
 const Joi = require('joi');
 
-const autoSchema = Joi.object({
+const autoSchema = Joi.object().keys({
     nomeCliente: Joi.string().min(5).max(50).required(),
     dataEntrega: Joi.date().required(),
     pontoPartida: Joi.string().min(10).required(),
--- routers/entregas.routers.js
+++ routers/entregas.routers.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
 const router = Router();
 const controller = require('../controller/entragas.controller');
+const { autoSchema } = require('../middlewares/validation_schema');
+const middleware = require('../middlewares/middlewarevalidatioin');
 
 router.get('/entregas', controller.findAll);
 router.get('/entregas/:id', controller.findOne);
-router.post('/entregas', controller.create);
+router.post('/entregas', middleware(autoSchema) ,controller.create);
 
 
 module.exports = router;
